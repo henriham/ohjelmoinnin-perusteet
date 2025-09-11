5ee9f05e286d86a8de8cb7628cffdf3c3ddd2ff2
Henri Hämynen <henri.hamynen@gmail.com>
2025-09-11 14:11:14 +0300

    koittaan toimivuutta

---
+++ week1/CheatSheet.py
@@ -0,0 +1,34 @@
+print("uusi printtaus")
+print("lisää printtii")
+
+print ("He said: \"Hello\", and kept walking ")
+print("He said hello \n \r \t \b and kept walking")
+
+exampleString = "Text and nub3rs"
+exampleInt = 123
+exampleFloat = 1.23
+exampleBoolean = True
+
+#Firstname = input("What is your firstname: ")
+#Lastname = input("What is your lastname: ")
+#print("Hello", Firstname, Lastname)
+
+Sentence = "Finding substring"
+print(Sentence)
+
+#print(Sentence[0]) # ensimmäinen kijrain
+#print(Sentence[-2]) # Toinen kirjain lopusta
+#print(Sentence[2:9]) # kolmas kirjain mukaan luettuna kaikki yhdeksänteen asti
+#print(Sentence[:9]) # printtaa yhdeksän merkkiä
+#print(Sentence[-9:-2])
+#print(Sentence[2:9:3]) # askeleet. aloitetaan 2 ysiin asti, kolmen askeleella
+#print(Sentence[::3]) # joka kolmas merkki
+#print(Sentence[::-1])
+
+#num1 = input("Anna luku 1: ")
+#num2 = input("Anna luku 2: ")
+
+#num3 = int(num1) + int(num2) # vaihtoehtoisesti int(input("Anna luku 1: "))
+#print(num3)
+
+print(len(Sentence))
