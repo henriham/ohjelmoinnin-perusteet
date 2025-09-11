d7c7a79e01cceab73c66ab793028e6e4e1ecc8fe
Henri Hämynen <henri.hamynen@gmail.com>
2025-09-11 15:05:38 +0300

    tehtävät tehty

---
+++ week1/A1_T5.py
@@ -0,0 +1,10 @@
+print("Calculate area of a wall")
+Feed = input("Enter width in meters: ")
+Width = int(Feed)
+Feed = input("Enter height in meters: ")
+Height = int(Feed)
+print(f"Width is {Width} m and height is {Height} m.")
+Area = Width * Height
+print(f"The wall will be {Area} square meters.")
+
+
---
+++ week1/A1_T7.py
@@ -0,0 +1,7 @@
+print("Calculta fuel consumption")
+Feed = input("Enter travel distance(kilometers): ")
+Distance = int(Feed)
+Feed = input("Enter fuel usage(liters): ")
+FuelUsage = int(Feed)
+Consumption = (FuelUsage / Distance) * 100
+print(f"Fuel consumption is {Consumption} l per 100 km")
