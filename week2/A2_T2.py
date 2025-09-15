0bfb14d9ae2c4a47b3906df80ffc8092ed65b428
Henri Hämynen <henri.hamynen@gmail.com>
2025-09-15 14:35:21 +0300

    little fix on line 1

--- week2/A2_T2.py
+++ week2/A2_T2.py
@@ -1,9 +1,9 @@
-
+print(f"Program starting.")
 carBrand = input("Insert car brand: ")
 carModel = input("Insert car model: ")
 
 #print(f'Car brand is "{carBrand}" and the model is \'{carModel}\'')
-print(f"Program starting.")
+
 print("Car", "brand", "is", '"'+carBrand+'"', sep=' ', end=' ')
 print("and", "the", "model", "is", "'"+carModel+"'.", sep=' ')
 print(f"Program ending.")
