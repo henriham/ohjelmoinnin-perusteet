cfa48444867ee2238bb21e034a8a5dccfc302890
Henri Hämynen <henri.hamynen@gmail.com>
2025-09-15 14:46:08 +0300

    task 3 done

---
+++ week2/A2_T3.py
@@ -0,0 +1,7 @@
+print(f"Program starting.")
+first_word = input("Insert first word: ")
+second_word = input("Inserts second word: ")
+print(f"1st word is {len(first_word)} characters long.")
+print(f"2nd word is {len(second_word)} characters long.")
+print(f"Words together makes one closed compound '{first_word+second_word}'.")
+print(f"Program ending.")
