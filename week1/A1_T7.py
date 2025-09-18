6d2fab5b8f58e704ffe7c7d6e2d0189c867ac85a
Henri Hämynen <henri.hamynen@gmail.com>
2025-09-18 12:48:05 +0300

    little fix

--- week1/A1_T7.py
+++ week1/A1_T7.py
@@ -3,6 +3,5 @@ Feed = input("Enter travel distance(kilometers): ")
 Distance = int(Feed)
 Feed = input("Enter fuel usage(liters): ")
 FuelUsage = int(Feed)
-Consumption = (FuelUsage / Distance) * 100
-Consumption = int(Consumption)
+Consumption = int((FuelUsage / Distance) * 100)
 print(f"Fuel consumption is {Consumption} l per 100 km")
