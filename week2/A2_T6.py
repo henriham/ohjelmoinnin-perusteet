b8dc55d35984eb3b484633738250ddda4ef1da0a
Henri Hämynen <henri.hamynen@gmail.com>
2025-09-18 15:44:21 +0300

    task 6 done

---
+++ week2/A2_T6.py
@@ -0,0 +1,14 @@
+print(f"Program starting.")
+print(f"")
+hex_color = input("Insert a hex color: ")
+red = hex_color[1:3:1]
+green = hex_color[3:5:1]
+blue = hex_color[5:7:1]
+
+print(f"")
+print(f"Colors")
+print(f"- Red {red}")
+print(f"- Green {green}")
+print(f"- Blue {blue}")
+print(f"")
+print(f"Program ending.")
