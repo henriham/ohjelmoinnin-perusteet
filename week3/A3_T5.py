90aee45d34c029d750be5cf10b6402fdd2a97d6a
Henri Hämynen <henri.hamynen@gmail.com>
2025-09-25 14:44:35 +0300

    task 5 added

---
+++ week3/A3_T5.py
@@ -0,0 +1,17 @@
+print("Program starting.")
+print("")
+print("Options: ")
+print("1 - Celsius to Fahrenheit")
+print("2 - Fahrenheit to Celsius")
+print("0 - Exit")
+user_choice = int(input("Your choice: "))
+if user_choice == 0: print("Exiting...")
+elif user_choice == 1:
+	user_input = float(input("Insert the amount of Celsius: "))
+	print(f"{user_input:.1f} °C equals to {1.8*user_input+32:.1f} °F")
+elif user_choice == 2:
+	user_input = float(input("Insert the amount of Fahrenheit: "))
+	print(f"{user_input:.1f} °F equals to {((user_input-32)/1.8):.1f} °C")
+print("")
+print("Program ending.")
+
