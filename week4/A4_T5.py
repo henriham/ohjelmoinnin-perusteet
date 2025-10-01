b4ea959e55c2f70260c436bc041d25e5b5afeba5
henriham <henri.hamynen@gmail.com>
2025-10-02 01:01:11 +0300

    fixes

--- week4/A4_T5.py
+++ week4/A4_T5.py
@@ -3,20 +3,21 @@ print("")
 start = int(input("Insert starting point: "))
 stop = int(input("Insert stopping point: "))
 inspect = int(input("Insert inspection point: "))
-string = ""
-for i in range(start, stop):
-  if inspect == i:
-    break
-  elif start >= stop:
-    print("")
-    print("Starting point value must be less than the stopping point value.")
-    break
-  elif inspect not in (i):
-    print("Inspection value must be within the range of start and stop.")
-    break
-  
-  elif i == inspect-1: string += str(f"{i }")
-  else: string += str(f"{i }")
 
-print(string)
+if start >= stop:
+  print("")
+  print("Starting point value must be less than the stopping point value.")
+elif (inspect < start) or (inspect > stop):
+  print("")
+  print("Inspection value must be within the range of start and stop.")
+else:
+  string = ""
+  for i in range(start, stop):
+    if inspect == i:
+      break
+    elif i == inspect-1: string += str(f"{i}")
+    else: string += str(f"{i} ")
+  print("First loop - inspection with break:")
+  print(string)
+print("")  
 print("Program ending.")
