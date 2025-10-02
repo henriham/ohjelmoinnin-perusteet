9c9e407084200140f99f670f32802322b6f71a92
Henri Hämynen <henri.hamynen@gmail.com>
2025-10-02 12:34:37 +0300

    spelling fix

--- week3/A3_T5.py
+++ week3/A3_T5.py
@@ -6,7 +6,6 @@ print("2 - Fahrenheit to Celsius")
 print("0 - Exit")
 user_choice = float(input("Your choice: "))
 if user_choice == 0:
-	print("")
 	print("Exiting...")
 elif user_choice == 1:
 	user_input = float(input("Insert the amount of Celsius: "))
