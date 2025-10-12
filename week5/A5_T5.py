44bddc769cfc489f5d4d1142814d7efeaef1b1e7
henriham <henri.hamynen@gmail.com>
2025-10-12 13:51:33 +0300

    fixed

--- week5/A5_T5.py
+++ week5/A5_T5.py
@@ -1,25 +1,27 @@
 print("Program starting.")
 current_word = ""
-
 def options():
   global current_word
+  if len(current_word) == 0:
+    print("")
   print("Options:")
   print("1 - Insert word")
   print("2 - Show current word")
   print("3 - Show current word in reverse")
   print("0 - Exit")
-  user_input = int(input("Your choice: "))
-  if user_input == 0:
+  user_input = input("Your choice: ")
+  int_input = int(user_input)
+  if int_input == 0:
     print("Exiting Program.")
     print("")
     print("Program ending")
-  elif user_input == 1:
+  elif int_input == 1:
     current_word = input("Insert word: ")
     options()
-  elif user_input == 2:
+  elif int_input == 2:
     print(f'Current word - "{current_word}"')
     options()
-  elif user_input == 3:
+  elif int_input == 3:
     print(f'Current word - "{current_word[::-1]}"')
     options()
   else:
