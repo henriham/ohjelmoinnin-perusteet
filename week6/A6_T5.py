4eff0c514db0abdf5effdafbf7017b26316a430a
henriham <henri.hamynen@gmail.com>
2025-10-31 23:28:38 +0200

    task 5 done

--- week6/A6_T5.py
+++ week6/A6_T5.py
@@ -1,26 +1,25 @@
 def filehandler():
-  filename = input('Insert filename: ')
+  filename = input("Insert filename: ")
   file = open(filename, 'r')
-  lines = file.readlines()
-
-
-
-
-
-
-
-
-
-
-
+  data = [int(x.strip()) for x in file.readlines()]
+  file.close()
+  return data, filename
+  
+def analyser():
+  data, filename = filehandler()
+  count = len(data)
+  summ = sum(data)
+  greatest = max(data)
+  average = summ / count
+  print("#### Number analysis - START ####")
+  print(f"File \"{filename}\" results:")
+  print("Count;Sum;Greatest;Average")
+  print(f"{count};{summ};{greatest};{average:.2f}")
+  print("\n#### Number analysis - END ####")
 
 def main():
   print("Program starting.")
-
-
-
-
-
+  analyser()
   print("Program ending.")
 
-
+main()
