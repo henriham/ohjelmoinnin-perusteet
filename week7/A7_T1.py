4afa177e168b2240a07d796f6f44866debafc659
henriham <henri.hamynen@gmail.com>
2025-11-30 05:13:02 +0200

    syntax fix

--- week7/A7_T1.py
+++ week7/A7_T1.py
@@ -3,6 +3,7 @@ def collectPosInt():
   feed = int(input("Insert positive integer(negative stops): "))
   return feed
 
+
 def main():
   print("Program starting.")
   print("Collcet positive integers.")
@@ -12,13 +13,19 @@ def main():
     feed = collectPosInt()
     if feed > -1:
       nums.append(feed)
-    else:
+    elif len(nums) == 0:
+      print("No integers to display.")
+      print("Stopped collecting positive integers.")
       break
-  print(f"Displaying {(len(nums))} integers:")
+    else:
+      print("Stopped collecting positive integers.")
+      break
+    if len(nums) > 0:
+      print(f"Displaying {(len(nums))} integers:")
   for i in range(len(nums)):
     print(f" Index {i} => Ordinal {i+1} => Integer {nums[i]}")
 
   print("Program ending.")
 
-if "__NAME__":
+if __name__ == "__main__":
   main()
